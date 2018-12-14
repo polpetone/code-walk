01e86a389d973a12cf04f5f4e12e0360ca54efc6
Stefan Heiden <stefan.heiden@qudosoft.de>
2018-12-14 23:02:49 +0100

    printing code from specific files

--- main.go
+++ main.go
@@ -4,29 +4,72 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"bufio"
+	"io/ioutil"
 	"path/filepath"
 )
 
-func visit(files *[]string) filepath.WalkFunc {
-    return func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            log.Fatal(err)
-        }
-        *files = append(*files, path)
-        return nil
+func getFiles(root string) []os.FileInfo {
+	files, err := ioutil.ReadDir(root)
+    if err != nil {
+        log.Fatal(err)
     }
+	return files
+}
+
+func visit(files *[]string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if !info.IsDir() {
+			*files = append(*files, path)
+		}
+		return nil
+	}
+}
+
+func readFile(fileName string){
+	file, err := os.Open(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+    }
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+	_, ok := set[item]
+	return ok
 }
 
 func main(){
 
+	fileTypes := []string{".tf", ".sh", ".java"}
 	var files []string
 
     root := "/home/icke/workspace/qudo"
-    err := filepath.Walk(root, visit(&files))
-    if err != nil {
-        panic(err)
-    }
+	err := filepath.Walk(root, visit(&files))
+	if err != nil {
+		panic(err)
+	}
     for _, file := range files {
-        fmt.Println(file)
+    	var extension = filepath.Ext(file)
+    	if contains(fileTypes, extension){
+			fmt.Println(file)
+			fmt.Println(extension)
+    		readFile(file)
+		}
     }
 }
