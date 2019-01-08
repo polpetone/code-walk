65905d4983b77eab1a5c30c50586383bcf017b9e
Stefan Heiden <stefan.heiden@qudosoft.de>
2019-01-08 20:03:37 +0100

    server poc

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"net/rpc"
 	"os"
 	"path/filepath"
 )
@@ -11,6 +14,16 @@ const DEFAULT_LOG = "/tmp/code_walk.log"
 var homeDir  = os.Getenv("HOME")
 var codeWalkDir = homeDir + "/.code_walk"
 var fileTypes = []string{".tf", ".sh", ".java", ".go"}
+
+
+type Command int
+
+
+func (c *Command) Receive(key string, reply *string) error {
+	fmt.Println("received key", key)
+	return nil
+}
+
 
 func initCodeWalk() {
 	Info.Println("create if not exist: ", codeWalkDir)
@@ -31,9 +44,27 @@ func main() {
 	initLogging(DEFAULT_LOG)
 	initCodeWalk()
 
-	dir := flag.String("dir", "/", "directory to walk")
-	flag.Parse()
-	files := readSrcFiles(*dir)
+	command := new(Command)
 
-	engine(files, fileTypes)
+	err := rpc.Register(command)
+
+	if err != nil {
+		fmt.Println("Format of service Command isn't correct", err)
+	}
+	rpc.HandleHTTP()
+	listener, e := net.Listen("tcp", ":8123")
+	if e != nil {
+		fmt.Println("Listen error: ", e)
+	}
+	fmt.Println("Serving RPC server on port ", 8123)
+	err = http.Serve(listener, nil)
+	if err != nil {
+		fmt.Println("Error serving: ", err)
+	}
+
+	//dir := flag.String("dir", "/", "directory to walk")
+	//flag.Parse()
+	//files := readSrcFiles(*dir)
+
+	//engine(files, fileTypes)
 }
